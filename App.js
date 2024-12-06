ffc1b73f7371e1cdf7243fd1b460677593016407
Raman Dhaliwal <dramandeepkaur202@gmail.com>
2024-12-06 13:31:00 -0600

    Update App.js to update file name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my Test app- on 6th dec., 2022</Text>
       <StatusBar style="auto" />
     </View>
   );
